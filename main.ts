ec651c691ff95535d4244d800ed2bde1b5f827dd
Stanislav Pugachev <129574544+stanislav-pugachev@users.noreply.github.com>
2023-04-01 11:14:57 +0600

    Обновить main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,100 +1,136 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 4; index++) {
-        basic.showIcon(IconNames.Skull)
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            # . # . #
-            # # # # #
-            . # # # .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . # # # .
-            # . # . #
-            # # # # #
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . # # # .
-            # . # . #
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . # # # .
-            `)
-        basic.clearScreen()
-        basic.pause(500)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . # # # .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . # # # .
-            # . # . #
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . # # # .
-            # . # . #
-            # # # # #
-            `)
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            # . # . #
-            # # # # #
-            . # # # .
-            `)
-        basic.showIcon(IconNames.Skull)
-    }
+    basic.showIcon(IconNames.Skull)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        # . # . #
+        # # # # #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        # . # . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # .
+        # . # . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # .
+        `)
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # .
+        # . # . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        # . # . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        # . # . #
+        # # # # #
+        . # # # .
+        `)
+    basic.showIcon(IconNames.Skull)
 })
 input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index < 4; index++) {
-        basic.showIcon(IconNames.Skull)
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            # . # . #
-            # # # # #
-            . # # # .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . # # # .
-            # . # . #
-            # # # # #
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . # # # .
-            # . # . #
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . # # # .
-            `)
-        basic.clearScreen()
-    }
+    basic.showIcon(IconNames.Skull)
+    basic.showLeds(`
+        # . # . #
+        # # # # #
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . # . #
+        # # # # #
+        . # # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . # . #
+        # # # # #
+        . # # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . # . #
+        # # # # #
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showIcon(IconNames.Skull)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.showIcon(IconNames.Square)
+    basic.clearScreen()
 })
 basic.forever(function () {
 	
